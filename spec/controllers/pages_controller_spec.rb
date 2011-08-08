68aa92da599024803e31515564ef231d19871928
Masakuni Kato <mackato@gmail.com>
2011-08-09 02:07:11 +0900

    controlerのジェネレーターが生成した不要なspecを削除

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,22 +4,12 @@ require 'spec_helper'
 describe PagesController do
 
   describe "#index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
-    end
-    
     it "GET pages_path にマッチ" do
       { :get => pages_path }.should route_to(:controller => "pages", :action => "index" )
     end
   end
 
   describe "#new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
-    
     it "GET new_page_path にマッチ" do
       { :get => new_page_path }.should route_to(:controller => "pages", :action => "new" )
     end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,11 +4,6 @@ require 'spec_helper'
 describe WelcomeController do
 
   describe "#index" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
-    end
-    
     it "GET root_path にマッチ" do
       { :get => root_path }.should route_to(
         :controller => "welcome",
