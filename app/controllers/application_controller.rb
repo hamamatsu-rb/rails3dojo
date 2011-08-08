3e3274998f8d7d07574709f4474b6eb9b3f6624a
Masakuni Kato <mackato@gmail.com>
2011-08-09 02:13:55 +0900

    ログインしていない時に毎回例外処理が走るのがイヤなのでifでブロックする処理を追加した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user
   rescue ActiveRecord::RecordNotFound => e
     nil
   end
