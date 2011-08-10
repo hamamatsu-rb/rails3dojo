36de3f45ada8035f3e6a63cda7d4f31325ee6acf
Masakuni Kato <mackato@gmail.com>
2011-08-10 11:34:12 +0900

    トップページでは'Home'というタイトルを持つページがある場合はそれを表示するように変更

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @page = Page.find_by_title("Home")
+    render :template => 'pages/show' if @page
   end
-
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,10 +5,38 @@ describe WelcomeController do
 
   describe "#index" do
     it "GET root_path にマッチ" do
-      { :get => root_path }.should route_to(
-        :controller => "welcome",
-        :action => "index" )
+      { :get => root_path }.should route_to(:controller => "welcome", :action => "index")
+    end
+    
+    context "'Home'というタイトルのPageが存在する場合" do
+      before do
+        home = create_page(:title => "Home")
+        create_page(:title => "Other", :user => home.user)
+        get :index
+      end
+      
+      it "テンプレート pages/show を表示する" do
+        response.should render_template('pages/show')
+      end
+      
+      it "'Home'を表示する" do
+        assigns(:page).title.should eql("Home")
+      end
+    end
+    
+    context "Page 'Home'が存在しない場合" do
+      before do
+        home = create_page(:title => "Other")
+        get :index
+      end
+      
+      it "テンプレート index を表示する" do
+        response.should render_template(:index)
+      end
+      
+      it "Pageはアサインされない" do
+        assigns(:page).should_not be
+      end
     end
   end
-
 end
