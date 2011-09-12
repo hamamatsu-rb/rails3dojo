76946577125f9ff8280ece72998f60461708fd0a
Masakuni Kato <mackato@gmail.com>
2011-09-12 12:33:26 +0900

    ログイン/ログアウト処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  helper_method :current_user
+  
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
     @current_user
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_tag
+    unless flash.empty?
+      flash.each do |k, v|
+        return content_tag(:div, v, :class => [k, :flash])
+      end
+    end
+  end
 end
