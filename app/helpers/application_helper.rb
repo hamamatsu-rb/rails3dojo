0ffc6013c7e6d9c41512cd2dddd657f80338fbed
Masakuni Kato <mackato@gmail.com>
2011-09-12 13:31:03 +0900

    Page一覧の画面処理を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
       end
     end
   end
+  
+  def page_update_info_tag(page)
+    user, dt = nil, nil
+    if page.histories.empty?
+      user = page.user
+      dt = page.created_at
+    else
+      history = page.histories.first
+      user = history.user
+      dt = history.created_at
+    end
+    raw("Last update by #{content_tag(:strong, user.name)} on #{content_tag(:strong, dt.to_s(:short))}")
+  end
 end
